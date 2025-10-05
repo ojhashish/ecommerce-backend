a7570c63b716f2e858981409899c554b30550b48
Ashish Kumar Ojha <96771171+ojhashish@users.noreply.github.com>
2025-10-05 12:57:31 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const app = express();
 app.use(bodyParser.json());
 
 app.use(cors({
-  origin: ['http://localhost:4200', 'https://ecommerce-frontendbiz2x.netlify.app/']
+  origin: ['*'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
